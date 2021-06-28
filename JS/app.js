9deee6f7118ede07d951a40678883b09dc9486a7
GinaHobbs <ginamariehobbs@gmail.com>
2021-06-28 17:56:05 -0500

    scaffolding partly complete!

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+// global variables/functions
+  // planet array
+  // counter for questions answered correctly
+  // html document.getElementById
+
+// We need a planet constructor
+// takes in name, image, info about the planet, quiz questions (in an array)
+
+// We need a function that when you click on a planet, it loads up the correct planet/info from the planet object array
+
+// prototype function that renders a planet and info to the page
+
+// We're going to need some quiz code for multiple choice and text input (using forms and buttons)
+
+// 
